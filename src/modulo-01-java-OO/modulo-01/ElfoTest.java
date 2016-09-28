2926a11a55ea5199d917d2feccfa06031447b0bb
Unknown <maiconloebens@gmail.com>
2016-09-28 15:54:07 -0300

    test(Elfo): adicionar novos testes de atirarFlecha.

--- src/modulo-01-java-OO/modulo-01/ElfoTest.java
+++ src/modulo-01-java-OO/modulo-01/ElfoTest.java
@@ -25,7 +25,7 @@ public class ElfoTest
     }
 
     @Test
-    public void elfoNasceComFlechas(){
+    public void elfoNasceCom42Flechas(){
         // Act
         Elfo elfoDoTeste = new Elfo("Legolas");
         // Assert
@@ -34,51 +34,98 @@ public class ElfoTest
     }
 
     @Test
-    public void elfoAtiraFlecha(){
+    public void elfoAtiraUmaFlechaEmUmDwarf(){
         // Act
         Elfo elfoDoTeste = new Elfo("Legolas");
-        Dwarf dwarf = new Dwarf("Tyrion");
+        Dwarf dwarf = new Dwarf();
         
         elfoDoTeste.atirarFlecha(dwarf);
         // Assert
-        assertEquals(1, elfoDoTeste.getArco().getQuantidade());
         assertEquals(41, elfoDoTeste.getFlecha().getQuantidade());
         assertEquals(1, elfoDoTeste.getExperiencia());
+        assertEquals(100, dwarf.getVida());
     }
 
     @Test
-    public void elfoAtiraVariasFlecha(){
-        // Act
+    public void elfoAtiraDuasFlechasEmUmDwarf(){
+        // Arrange
         Elfo elfoDoTeste = new Elfo("Legolas");
-        Dwarf dwarf = new Dwarf("Tyrion");
-        
-        elfoDoTeste.atirarFlecha(dwarf);
-        elfoDoTeste.atirarFlecha(dwarf);
+        Dwarf dwarf = new Dwarf();
+        // Act
         elfoDoTeste.atirarFlecha(dwarf);
         elfoDoTeste.atirarFlecha(dwarf);
         // Assert
-        assertEquals(1, elfoDoTeste.getArco().getQuantidade());
-        assertEquals(38, elfoDoTeste.getFlecha().getQuantidade());
-        assertEquals(4, elfoDoTeste.getExperiencia());
+        assertEquals(40, elfoDoTeste.getFlecha().getQuantidade());
+        assertEquals(2, elfoDoTeste.getExperiencia());
+        assertEquals(90, dwarf.getVida());
     }
 
-    @Test
-    public void elfoNaoAtiraFlecha(){
-        // Act
+        @Test
+    public void elfoAtira42FlechasEm3Dwarves(){
+        // Arrange
         Elfo elfoDoTeste = new Elfo("Legolas");
+        Dwarf dwarfTyrion = new Dwarf();
+        Dwarf dwarfMinimim = new Dwarf();
+        Dwarf dwarfGimli = new Dwarf();
+        // Act
+        elfoDoTeste.atirarFlecha(dwarfTyrion);
+        elfoDoTeste.atirarFlecha(dwarfTyrion);
+        elfoDoTeste.atirarFlecha(dwarfMinimim);
+        elfoDoTeste.atirarFlecha(dwarfGimli);
+        elfoDoTeste.atirarFlecha(dwarfGimli);
+        elfoDoTeste.atirarFlecha(dwarfMinimim);
+        elfoDoTeste.atirarFlecha(dwarfGimli);
+        elfoDoTeste.atirarFlecha(dwarfTyrion);
+        elfoDoTeste.atirarFlecha(dwarfMinimim);
+        elfoDoTeste.atirarFlecha(dwarfTyrion);
+        elfoDoTeste.atirarFlecha(dwarfMinimim);
+        elfoDoTeste.atirarFlecha(dwarfGimli);
+        elfoDoTeste.atirarFlecha(dwarfTyrion);
+        elfoDoTeste.atirarFlecha(dwarfGimli);
+        elfoDoTeste.atirarFlecha(dwarfGimli);
+        elfoDoTeste.atirarFlecha(dwarfMinimim);
+        elfoDoTeste.atirarFlecha(dwarfTyrion);
+        elfoDoTeste.atirarFlecha(dwarfGimli);
+        elfoDoTeste.atirarFlecha(dwarfTyrion);
+        elfoDoTeste.atirarFlecha(dwarfGimli);
+        elfoDoTeste.atirarFlecha(dwarfMinimim);
+        elfoDoTeste.atirarFlecha(dwarfMinimim);
+        elfoDoTeste.atirarFlecha(dwarfGimli);
+        elfoDoTeste.atirarFlecha(dwarfTyrion);
+        elfoDoTeste.atirarFlecha(dwarfMinimim);
+        elfoDoTeste.atirarFlecha(dwarfMinimim);
+        elfoDoTeste.atirarFlecha(dwarfMinimim);
+        elfoDoTeste.atirarFlecha(dwarfGimli);
+        elfoDoTeste.atirarFlecha(dwarfTyrion);
+        elfoDoTeste.atirarFlecha(dwarfTyrion);
+        elfoDoTeste.atirarFlecha(dwarfGimli);
+        elfoDoTeste.atirarFlecha(dwarfGimli);
+        elfoDoTeste.atirarFlecha(dwarfTyrion);
+        elfoDoTeste.atirarFlecha(dwarfTyrion);
+        elfoDoTeste.atirarFlecha(dwarfMinimim);
+        elfoDoTeste.atirarFlecha(dwarfTyrion);
+        elfoDoTeste.atirarFlecha(dwarfGimli);
+        elfoDoTeste.atirarFlecha(dwarfTyrion);
+        elfoDoTeste.atirarFlecha(dwarfMinimim);
+        elfoDoTeste.atirarFlecha(dwarfTyrion);
+        elfoDoTeste.atirarFlecha(dwarfGimli);
+        elfoDoTeste.atirarFlecha(dwarfTyrion);
         // Assert
-        assertEquals(42, elfoDoTeste.getFlecha().getQuantidade());
+        assertEquals(0, elfoDoTeste.getFlecha().getQuantidade());
+        assertEquals(42, elfoDoTeste.getExperiencia());
+        assertEquals(-50, dwarfTyrion.getVida());
+        assertEquals(-30, dwarfGimli.getVida());
+        assertEquals(-10, dwarfMinimim.getVida());
     }
-
+    
     @Test
     public void elfoNaoTemFlechaNegativa(){
-        // Act
+        // Arrange
         Elfo elfoDoTeste = new Elfo("Legolas");
-        Dwarf dwarfTyrion = new Dwarf("Tyrion");
-        Dwarf dwarfMinimim = new Dwarf("Minimim");
-        Dwarf dwarfGimli = new Dwarf("Gimli");
-        
-        // Podia liberar laço de repetição...
+        Dwarf dwarfTyrion = new Dwarf();
+        Dwarf dwarfMinimim = new Dwarf();
+        Dwarf dwarfGimli = new Dwarf();
+        // Act
         elfoDoTeste.atirarFlecha(dwarfTyrion);
         elfoDoTeste.atirarFlecha(dwarfTyrion);
         elfoDoTeste.atirarFlecha(dwarfMinimim);
@@ -125,5 +172,14 @@ public class ElfoTest
         elfoDoTeste.atirarFlecha(dwarfMinimim);
         // Assert
         assertEquals(0, elfoDoTeste.getFlecha().getQuantidade());
+    }
+    
+    @Test
+    public void elfoNaoAtiraFlecha(){
+        // Act
+        Elfo elfoDoTeste = new Elfo("Legolas");
+        // Assert
+        assertEquals(42, elfoDoTeste.getFlecha().getQuantidade());
+        assertEquals(0, elfoDoTeste.getExperiencia());
     }
 }
