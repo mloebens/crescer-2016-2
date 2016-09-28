ad0669494dd8248ce00452729b3453f57e0351cc
Máicon Loebens <maiconloebens@gmail.com>
2016-09-27 23:36:02 -0300

    test(CestoDeLembas): adicionar testes para criação e funcionalidade

---
+++ src/modulo-01-java-OO/modulo-01/CestoDeLembasTest.java
@@ -0,0 +1,65 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class CestoDeLembasTest
+{
+   @Test
+   public void criarCestoDeLembas(){
+       
+        // Act
+        CestoDeLembas lembas = new CestoDeLembas(15);
+        // Assert
+        assertEquals(15, lembas.getQuantidade());
+    }
+    
+    @Test
+    public void dividirDuasLembasEmPares(){
+        // Act
+        CestoDeLembas lembas = new CestoDeLembas(2);
+        // Assert
+        assertEquals(false, lembas.podeDividirEmPares());
+    }
+    
+    @Test
+    public void dividirUmaLembasEmPares(){
+        // Act
+        CestoDeLembas lembas = new CestoDeLembas(1);
+        // Assert
+        assertEquals(false, lembas.podeDividirEmPares());
+    }
+    
+    @Test
+    public void dividirZeroLembasEmPares(){
+        // Act
+        CestoDeLembas lembas = new CestoDeLembas(0);
+        // Assert
+        assertEquals(false, lembas.podeDividirEmPares());
+    }
+    
+    @Test
+    public void dividirCemLembasEmPares(){
+        // Act
+        CestoDeLembas lembas = new CestoDeLembas(100);
+        // Assert
+        assertEquals(true, lembas.podeDividirEmPares());
+    }
+    
+    @Test
+    public void dividirSessentaLembasEmPares(){
+        // Act
+        CestoDeLembas lembas = new CestoDeLembas(60);
+        // Assert
+        assertEquals(true, lembas.podeDividirEmPares());
+    }
+    
+    @Test
+    public void dividirCinquentaUmLembasEmPares(){
+        // Act
+        CestoDeLembas lembas = new CestoDeLembas(51);
+        // Assert
+        assertEquals(false, lembas.podeDividirEmPares());
+    }
+}
