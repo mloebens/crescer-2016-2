4e64f62d09cbc341bc9336ff2e1313126902e1db
Máicon Loebens <maiconloebens@gmail.com>
2016-10-05 23:41:58 -0300

    feat(HobbitContador): adicionar funcionalidade obterMaiorMultiploDeTresAte e obterMultiplosDeTresAte.

--- src/modulo-01-java-OO/modulo-01/HobbitContador.java
+++ src/modulo-01-java-OO/modulo-01/HobbitContador.java
@@ -27,4 +27,31 @@ public class HobbitContador
         }  
         return valor1;
     }
+
+    public int obterMaiorMultiploDeTresAte(int valor){
+        int maior = 0;
+        //Quando procurar o maior valor, é mais fácil começar a iteração com o menor valor e então decrementar o contador.
+        for(int i = valor; i > 0; i--){
+            if(i % 3 == 0){
+                maior = i;
+                //o continue ignora o resto da execução dentro do laço e pula para a proxima iteração do laço
+                break;
+            }
+        }
+        return maior;
+    }
+
+    public ArrayList<Integer> obterMultiplosDeTresAte(int numero) {
+        ArrayList<Integer> multiplos = new ArrayList<>(Arrays.asList(0));
+
+        //o limite do for deve ser o numero recebido como parametro, pois os multiplos devem ser até esse número
+        for (int i = 1; i <= numero; i++) {
+            if (i % 3 == 0){
+                //o break interrompe o laço, ou seja, na primeira vez que a condição fosse verdadeira, o laço seria interrompido.
+                multiplos.add(i);
+            }
+        }
+
+        return multiplos;
+    }
 }
--- src/modulo-01-java-OO/modulo-01/HobbitContadorTest.java
+++ src/modulo-01-java-OO/modulo-01/HobbitContadorTest.java
@@ -44,7 +44,6 @@ public class HobbitContadorTest
 
     @Test
     public void calcularDiferenca3Pares(){
-
         ArrayList<ArrayList<Integer>> arrayDePares = new ArrayList<>();
         arrayDePares.add(new ArrayList<>(Arrays.asList(15, 18)));
         arrayDePares.add(new ArrayList<>(Arrays.asList(4, 5)));
@@ -54,4 +53,71 @@ public class HobbitContadorTest
 
         assertEquals(840, contador.calcularDiferenca(arrayDePares));
     }
+
+    @Test
+    public void obterMaiorMultiploDeTresAteTres(){
+        HobbitContador contador = new HobbitContador();
+
+        assertEquals(3, contador.obterMaiorMultiploDeTresAte(3));
+    }
+
+    @Test
+    public void obterMaiorMultiploDeTresAteDez(){
+        HobbitContador contador = new HobbitContador();
+
+        assertEquals(9, contador.obterMaiorMultiploDeTresAte(10));
+    }
+
+    @Test
+    public void obterMaiorMultiploDeTresAte20(){
+        HobbitContador contador = new HobbitContador();
+
+        assertEquals(18, contador.obterMaiorMultiploDeTresAte(20));
+    }
+
+    @Test
+    public void obterMaiorMultiploDeTresAteUm(){
+        HobbitContador contador = new HobbitContador();
+
+        assertEquals(0, contador.obterMaiorMultiploDeTresAte(1));
+    }
+
+    @Test
+    public void obterMultiplosDeTresAte10(){
+        HobbitContador contador = new HobbitContador();
+
+        assertEquals("[0, 3, 6, 9]", contador.obterMultiplosDeTresAte(10).toString());
+    }
+
+    @Test
+    public void obterMultiplosDeTresAte23(){
+        HobbitContador contador = new HobbitContador();
+
+        assertEquals("[0, 3, 6, 9, 12, 15, 18, 21]", contador.obterMultiplosDeTresAte(23).toString());
+    }
+
+    @Test
+    public void obterMultiplosDeTresAte30(){
+        HobbitContador contador = new HobbitContador();
+
+        assertEquals("[0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30]", contador.obterMultiplosDeTresAte(30).toString());
+    }
+    
+        @Test
+    public void obterMultiplosDeTresAteZero(){
+        HobbitContador contador = new HobbitContador();
+
+        assertEquals("[0]", contador.obterMultiplosDeTresAte(0).toString());
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
