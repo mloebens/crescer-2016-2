50014f85a3ed4c12a0508a46e0e02be1eb09da9a
Máicon Loebens <maiconloebens@gmail.com>
2016-10-05 23:51:33 -0300

    feat(ExercitoElfos): adicionar método getContingente.

--- src/modulo-01-java-OO/modulo-01/ExercitoElfos.java
+++ src/modulo-01-java-OO/modulo-01/ExercitoElfos.java
@@ -2,23 +2,27 @@ import java.util.ArrayList;
 
 public class ExercitoElfos
 {
-    private ArrayList<Elfo> exercito;
+    private ArrayList<Elfo> contingente;
 
     public ExercitoElfos(){
-        exercito = new ArrayList<>();
+        contingente = new ArrayList<>();
+    }
+
+    public Elfo[] getContingente() {
+        return contingente.toArray(new Elfo[contingente.size()]);
     }
 
     public void alistar(Elfo elfo){
-        
+
         boolean podeAlistar = elfo instanceof ElfoVerde || elfo instanceof ElfoNoturno;
-        
+
         if(podeAlistar){
-            exercito.add(elfo);
+            contingente.add(elfo);
         }
     }
 
     public Elfo buscar(String nomeElfo){
-        for(Elfo elfo : exercito){
+        for(Elfo elfo : contingente){
             if(nomeElfo != null && nomeElfo.equals(elfo.getNome())){
                 return elfo;
             }
@@ -29,7 +33,7 @@ public class ExercitoElfos
     public ArrayList<Elfo> buscar(Status status){
         ArrayList<Elfo> resultado = new ArrayList<>();
 
-        for(Elfo elfo : exercito){
+        for(Elfo elfo : contingente){
             if(elfo.getStatus() == status){
                 resultado.add(elfo);
             }
--- src/modulo-01-java-OO/modulo-01/ExercitoElfosTest.java
+++ src/modulo-01-java-OO/modulo-01/ExercitoElfosTest.java
@@ -11,88 +11,132 @@ public class ExercitoElfosTest {
         System.gc();
     }
 
-    @Test
-    public void alistarEBuscarElfoVerdePeloNome(){
+   @Test
+    public void alistarUmElfoVerde() {
         ExercitoElfos exercito = new ExercitoElfos();
-
-        exercito.alistar(new ElfoVerde("Legolas"));
-        assertEquals("Legolas", exercito.buscar("Legolas").getNome());
+        Elfo elfoVerde = new ElfoVerde("Elfo Verde");
+        exercito.alistar(elfoVerde);
+        assertEquals(elfoVerde, exercito.getContingente()[0]);
     }
 
     @Test
-    public void alistarEBuscarElfoNoturnoPeloNome(){
+    public void alistarUmElfoNoturno() {
         ExercitoElfos exercito = new ExercitoElfos();
-
-        exercito.alistar(new ElfoNoturno("Legolas"));
-        assertEquals("Legolas", exercito.buscar("Legolas").getNome());
+        Elfo elfoNoturno = new ElfoNoturno("Night Elf");
+        exercito.alistar(elfoNoturno);
+        assertEquals(elfoNoturno, exercito.getContingente()[0]);
     }
 
     @Test
-    public void alistarElfoNaoDeveFuncionar(){
+    public void alistarUmElfoNormal() {
         ExercitoElfos exercito = new ExercitoElfos();
-
-        exercito.alistar(new Elfo("Legolas"));
-        assertNull(exercito.buscar("Legolas"));
+        Elfo elfoNormal = new Elfo("Elfo Normal");
+        exercito.alistar(elfoNormal);
+        assertEquals(0, exercito.getContingente().length);
     }
 
     @Test
-    public void alistarEBuscarElfosPeloNome(){
+    public void alistarDosTresTiposSoEntramVerdesENoturnos() {
         ExercitoElfos exercito = new ExercitoElfos();
-
-        exercito.alistar(new ElfoNoturno("Elfo Noturno"));
-        exercito.alistar(new ElfoVerde("Elfo Verde"));
-
-        assertEquals("Elfo Noturno", exercito.buscar("Elfo Noturno").getNome());
-        assertEquals("Elfo Verde", exercito.buscar("Elfo Verde").getNome());
+        Elfo elfoVerde = new ElfoVerde("Elfo Verde");
+        Elfo elfoNormal = new Elfo("Elfo Normal");
+        Elfo elfoNoturno = new ElfoNoturno("Night Elf");
+        exercito.alistar(elfoVerde);
+        exercito.alistar(elfoNormal);
+        exercito.alistar(elfoNoturno);
+        assertEquals(2, exercito.getContingente().length);
+        assertEquals(elfoVerde, exercito.getContingente()[0]);
+        assertEquals(elfoNoturno, exercito.getContingente()[1]);
     }
 
     @Test
-    public void buscarPrimeiroElfoQuandoNomesIguais(){
-        ExercitoElfos exercito = new ExercitoElfos();
-
-        ElfoVerde recruta = new ElfoVerde("Elfo");
-        exercito.alistar(recruta);
-        exercito.alistar(new ElfoVerde("Elfo"));
-
-        assertEquals(recruta, exercito.buscar("Elfo"));
-
+    public void buscarExercitoVazio() {
+        assertNull(new ExercitoElfos().buscar("Nenhum"));
     }
 
     @Test
-    public void buscarElfoVivo(){
+    public void buscarPorNomeTendoApenasUmComAqueleNome() {
         ExercitoElfos exercito = new ExercitoElfos();
-
-        Elfo recruta1 = new ElfoVerde("Elfo 1");
-        Elfo recruta2 = new ElfoVerde("Elfo 2");
+        Elfo recruta1 = new ElfoVerde("Elfo Recruta 1");
+        Elfo recruta2 = new ElfoNoturno("Elfo Recruta 2");
+        Elfo recruta3 = new ElfoVerde("Elfo Recruta 3");
         exercito.alistar(recruta1);
         exercito.alistar(recruta2);
-
-        ArrayList<Elfo> elfos = exercito.buscar(Status.VIVO);
-
-        assertEquals(2, elfos.size());
-        assertEquals(recruta1, elfos.get(0));
-        assertEquals(recruta2, elfos.get(1));
-
+        exercito.alistar(recruta3);
+        assertEquals(recruta2, exercito.buscar("Elfo Recruta 2"));
     }
 
     @Test
-    public void buscarElfoMorto(){
+    public void buscarPorNomeTendoVariosComAqueleNome() {
         ExercitoElfos exercito = new ExercitoElfos();
-
-        Elfo recruta1 = new ElfoNoturno("Elfo 1",200);
-        Elfo recruta2 = new ElfoVerde("Elfo 2");
+        Elfo recruta1 = new ElfoVerde("Elfo Recruta");
+        Elfo recruta2 = new ElfoNoturno("Elfo Recruta 2");
+        Elfo recruta3 = new ElfoVerde("Elfo Recruta");
         exercito.alistar(recruta1);
         exercito.alistar(recruta2);
-
-        Dwarf dwarfAlvo = new Dwarf();
-        while(recruta1.getStatus() == Status.VIVO){
-            recruta1.atirarFlecha(dwarfAlvo);
-        }
-
-        ArrayList<Elfo> elfos = exercito.buscar(Status.MORTO);
-
-        assertEquals(1, elfos.size());
-        assertEquals(recruta1, elfos.get(0));
+        exercito.alistar(recruta3);
+        assertEquals(recruta1, exercito.buscar("Elfo Recruta"));
     }
+
+    @Test
+    public void buscarPorStatusVivo() {
+        ExercitoElfos exercito = new ExercitoElfos();
+        Elfo recruta1 = new ElfoVerde("Elfo Recruta");
+        Elfo recruta2 = new ElfoNoturno("Elfo Recruta 2");
+        Elfo recruta3 = new ElfoVerde("Elfo Recruta");
+        exercito.alistar(recruta1);
+        exercito.alistar(recruta2);
+        exercito.alistar(recruta3);
+        ArrayList<Elfo> resultado = exercito.buscar(Status.VIVO);
+        assertEquals(3, resultado.size());
+        assertEquals(recruta1, resultado.get(0));
+        assertEquals(recruta2, resultado.get(1));
+        assertEquals(recruta3, resultado.get(2));
+    }
+
+    @Test
+    public void buscarPorStatusMorto() {
+        ExercitoElfos exercito = new ExercitoElfos();
+        Elfo recruta1 = new ElfoVerde("Elfo Recruta");
+        Elfo recruta2 = criarElfoNoturnoEMatalo();
+        Elfo recruta3 = new ElfoVerde("Elfo Recruta");
+        exercito.alistar(recruta1);
+        exercito.alistar(recruta2);
+        exercito.alistar(recruta3);
+        ArrayList<Elfo> resultado = exercito.buscar(Status.MORTO);
+        assertEquals(1, resultado.size());
+        assertEquals(recruta2, resultado.get(0));
+    }
+    
+    @Test
+    public void buscarPorStatusMortoNenhumMorto() {
+        ExercitoElfos exercito = new ExercitoElfos();
+        Elfo recruta1 = new ElfoVerde("Elfo Recruta");
+        Elfo recruta2 = new ElfoNoturno("Elfo Recruta 2");
+        Elfo recruta3 = new ElfoVerde("Elfo Recruta");
+        exercito.alistar(recruta1);
+        exercito.alistar(recruta2);
+        exercito.alistar(recruta3);
+        assertTrue(exercito.buscar(Status.MORTO).isEmpty());
+    }
+
+    @Test
+    public void buscarPorStatusVivoNenhumVivo() {
+        ExercitoElfos exercito = new ExercitoElfos();
+        Elfo recruta1 = criarElfoNoturnoEMatalo();
+        Elfo recruta2 = criarElfoNoturnoEMatalo();
+        Elfo recruta3 = criarElfoNoturnoEMatalo();
+        exercito.alistar(recruta1);
+        exercito.alistar(recruta2);
+        exercito.alistar(recruta3);
+        assertTrue(exercito.buscar(Status.VIVO).isEmpty());
+    }
+    
+    private ElfoNoturno criarElfoNoturnoEMatalo() {
+        ElfoNoturno suicida = new ElfoNoturno("Elfo kamikaze", 90);
+        for (int i = 0; i < 90; i++)
+            suicida.atirarFlecha(new Dwarf());
+        return suicida;
+}
 
 }
