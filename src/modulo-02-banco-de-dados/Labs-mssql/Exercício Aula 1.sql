05fecbffec0f12671ffb6856eda8a90e5f776a69
Unknown <maiconloebens@gmail.com>
2016-10-11 14:37:31 -0300

    fix(Exercício Aula 1): corrigir table produto.

--- src/modulo-02-banco-de-dados/Labs-mssql/Exercício Aula 1.sql
+++ src/modulo-02-banco-de-dados/Labs-mssql/Exercício Aula 1.sql
@@ -12,19 +12,19 @@ insert into CidadeAux (IDCidade, Nome, UF)
 
 -- 3
 create table Produto (
-	IDProduto bigint identity,
-	NomeCurto varchar(20) not null,
-	NomeDescritivo varchar(100) not null,
-	DataCriacao datetime not null,
-	LocalEstoque varchar(50) not null,
-	Quantidade int not null,
-	Preco decimal(10,2) not null
+	IDProduto		bigint identity,
+	NomeCurto		varchar(20) not null,
+	NomeDescritivo	varchar(100) not null,
+	DataCriacao		datetime default getDate() not null,
+	LocalEstoque	varchar(50) not null,
+	Quantidade		decimal(7,3) not null,
+	Preco			decimal(7,2) not null
 		constraint PK_Produto primary key (IDProduto)
 );
 
 -- 4
 
-insert into produto(NomeCurto, NomeDescritivo, DataCriacao, LocalEstoque, Quantidade, Preco) 
-	values ('Televisão', 'Televisão SmartTv Samsung 50 polegadas.', current_timestamp, 'Corredor B2', 4, 4000.99 );
-insert into produto(NomeCurto, NomeDescritivo, DataCriacao, LocalEstoque, Quantidade, Preco) 
-	values ('Computador', 'Acer 14" Intel i5 2.2GHz 8GB mémoria ram, HD 500GB', current_timestamp, 'Corredor C1', 2, 2300.32 );
+insert into produto(NomeCurto, NomeDescritivo, LocalEstoque, Quantidade, Preco) 
+	values ('Televisão', 'Televisão SmartTv Samsung 50 polegadas.', 'Corredor B2', 4.0, 4000.99 );
+insert into produto(NomeCurto, NomeDescritivo, LocalEstoque, Quantidade, Preco) 
+	values ('Computador', 'Acer 14" Intel i5 2.2GHz 8GB mémoria ram, HD 500GB', 'Corredor C1', 2.0, 2300.32 );
