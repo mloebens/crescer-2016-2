27ca863321db25ce0c55086622466e2d7bfeb099
Máicon Loebens <maiconloebens@gmail.com>
2016-10-25 02:44:00 -0200

    feat(Exercício): implementar exercício.

--- src/modulo-04-javascript/dia-01/Exercício/exercicio.js
+++ src/modulo-04-javascript/dia-01/Exercício/exercicio.js
@@ -49,29 +49,62 @@ var find = function(arrayDeElementos, funcao){
   return resultado;
 }
 
-var maiorIgualADois = function(elemento) {
-  return elemento >= 2;
-};
-
-console.log(find([ 1, 2, 3 ], maiorIgualADois));
-
-console.log(find([ 'a', 5, null, false, 'false' ], function(elem) {
-  return typeof elem === 'string';
-}));
-// [ 'a', 'false' ]
-
-console.log(find([ { nome: 'a' }, { nome: 'b' }  ], function(elem) {
-  return typeof elem.nome === 'c';
-}));
-// []
-var maiorIgualADois = 'maior igual ou a dois';
-console.log(find([ 1, 2, 3 ], maiorIgualADois));
-
 //Exercício 4
-var subtrair = function(valor){
-  
+var subtrair = function(valor1){
+  return function(valor2){
+    return valor1 - valor2;
+  }
 }
 
-console.log(subtrair(2)(1)); // 1
-console.log(subtrair(0)(0)); // 0
-console.log(subtrair(-1)(-2)); // -3
+
+//Exercício 5
+var iguais = function(x, y){
+  if(x === y) return true;
+
+  if( !(x instanceof Object) || !(y instanceof Object )) return false;
+
+  for(var indice in x) {
+    if (!x.hasOwnProperty(indice)) continue;
+
+    if (!y.hasOwnProperty(indice)) return false;
+
+    if (x[indice] === y[indice]) continue;
+
+    if (typeof(x[indice]) !== "object") return false;
+
+    if (!iguais(x[indice],  y[indice])) return false;
+  }
+
+  for ( indice in y ) {
+    if (y.hasOwnProperty(indice) && !x.hasOwnProperty(indice)) return false;
+  }
+  return true;
+}
+
+//Exercício 6
+var ctrlC = function(valor){
+  var copia = Object.assign({},valor);
+  return copia;
+}
+
+
+//Exercício 6
+var mesclar = function(objeto1, objeto2, recursivo){
+  
+    for (var indice in objeto2){
+      var objeto2TemAtributo = objeto2.hasOwnProperty(indice);
+      var atributoDoobjeto2EObjeto = typeof objeto2[indice] === "object";
+      if(!!recursivo){
+        if (objeto2TemAtributo) {
+
+          if(atributoDoobjeto2EObjeto){
+              mesclar(objeto1[indice], objeto2[indice], true);
+          }
+        }
+      }
+      if(!recursivo || !atributoDoobjeto2EObjeto ){
+        objeto1[indice] = objeto2[indice];
+      }
+  }
+}
+
