57190cc70d9dcc8a5d45ab84ba502bb0ee1c5336
Máicon Loebens <maiconloebens@gmail.com>
2016-10-27 22:37:10 -0200

    feat(TerraMedia): exemplo da aula 1.

---
+++ src/modulo-05-.net/Projects/TerraMedia/TerraMediaTests/OrcTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Personagens;
+
+namespace TerraMediaTests
+{
+    [TestClass]
+    public class OrcTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var orc = new Orc();
+
+            Assert.AreEqual(12, orc.Ataque);
+        }
+    }
+}
