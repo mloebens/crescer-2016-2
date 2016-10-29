dc8eab150d13e8a0372ab0487496e75035aa6f66
Máicon Loebens <maiconloebens@gmail.com>
2016-10-29 19:56:55 -0200

    feat(SkyFusion): implementar kart/equipamento skyfusion.

---
+++ src/modulo-05-.net/Projects/MarioKart/MarioKart/Karts/SkyFusion.cs
@@ -0,0 +1,62 @@
+﻿using MarioKart.Equipamentos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioKart.Karts
+{
+    public class SkyFusion : Kart, IEquipamento
+    {
+
+        public SkyFusion(Corredor corredor) : base(corredor)
+        {
+        }
+
+        public int BonusDeVelocidade
+        {
+            get
+            {
+                int bonusExtra = 5;
+                int bonusVelocidade = this.bonusDeEquipamento() + this.bonusDeNivelDeCorredor();
+                bool adicionarBonusExtra = bonusVelocidade > 12;
+
+                if (adicionarBonusExtra)
+                {
+                    bonusVelocidade += bonusExtra;
+                }
+
+                return bonusVelocidade;
+            }
+        }
+
+
+        private int bonusDeEquipamento()
+        {
+            int umPontoDeBonusPorEquipamento = this.Equipamentos.Count;
+            return umPontoDeBonusPorEquipamento;
+        }
+
+        private int bonusDeNivelDeCorredor()
+        {
+            int bonusNoob = 1;
+            int bonusMediano = 2;
+            int bonusProfissional = 3;
+            bool nivelDeHabilidadeNoob = this.Corredor.NivelDeHabilidade == NivelDeHabilidade.Noob;
+            bool nivelDeHabilidadeMediano = this.Corredor.NivelDeHabilidade == NivelDeHabilidade.Mediano;
+
+            if (nivelDeHabilidadeNoob)
+            {
+                return bonusNoob;
+            }
+                
+            if (nivelDeHabilidadeMediano)
+            {
+                return bonusMediano;
+            }
+               
+            return bonusProfissional;
+        }
+    }
+}
