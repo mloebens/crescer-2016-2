4af12cfe048caf2703124f946edcbc9bf62100e2
Unknown <maiconloebens@gmail.com>
2016-10-31 17:36:05 -0200

    feat(StreetFighter): implementar lógica inicial - Incompleto.

---
+++ src/modulo-05-dot-net/aula-03/StreetFighter.Web - Exercício/StreetFighter.Web/StreetFighter.Web/Controllers/StreetFighterController.cs
@@ -0,0 +1,47 @@
+﻿using StreetFighter.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StreetFighter.Web.Controllers
+{
+    public class StreetFighterController : Controller
+    {
+        // GET: StreetFighter
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: FichaTecnica
+        public ActionResult FichaTecnica()
+        {
+            var personagem = new PersonagemModel();
+
+            personagem.Nome = "Blanka";
+            personagem.PrimeiraAparicao = "Street Fighter II The World Warrior (1991)";
+            personagem.Nascimento = new DateTime(1966,2,12);
+            personagem.Altura = 192;
+            personagem.Peso = 96;
+            personagem.Medidas = "B198, C120, Q172";
+            personagem.TipoSanguineo = "B";
+            personagem.HabilidadesEspeciais = "Caçar, Eletricidade";
+            personagem.Gosta = "Frutas tropicais, Pirarucu, Sua mãe";
+            personagem.Desgosta = "Army ants (espécie de formiga)";
+            personagem.EstiloDeLuta = "Luta Selvagem autodidata (Army Ants) / Capoeira";
+            personagem.Origem = "Brasil (lugar de nascença é provável como sendo Tailândia)";
+            personagem.FalaDaVitoria = "Ver você em ação é uma piada!";
+            personagem.SFF2Nickname = "A selvagem criança da natureza";
+            personagem.SFA3Nickname = "A animal pessoa amazônica";
+            personagem.SF4Nickname = "Guerreiro da selva";
+            personagem.SFA3Stage = "Ramificação do Rio Madeira - pantano, Brasil (ramificação do rio Madeira: talvez possa ser Mato Grosso, ou Tocantins?)";
+            personagem.SF2Stage = "Bacia do rio Amazonas (Brasil)";
+            personagem.GolpesEspeciaisFamosos = "Electric Thunder, Rolling Attack";
+            personagem.Imagem = "blanka.png";
+
+            return View();
+        }
+    }
+}
---
+++ src/modulo-05-dot-net/aula-03/StreetFighter.Web - Exercício/StreetFighter.Web/StreetFighter.Web/Models/PersonagemModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StreetFighter.Web.Models
+{
+    public class PersonagemModel
+    {
+        public string Nome { get; set; }
+        public string PrimeiraAparicao { get; set; }
+        public DateTime Nascimento { get; set; }
+        public int Altura { get; set; }
+        public double Peso { get; set; }
+        public string Medidas { get; set; }
+        public string TipoSanguineo { get; set; }
+        public string HabilidadesEspeciais { get; set; }
+        public string Gosta { get; set; }
+        public string Desgosta { get; set; }
+        public string EstiloDeLuta { get; set; }
+        public string Origem { get; set; }
+        public string FalaDaVitoria { get; set; }
+        public string SFF2Nickname { get; set; }
+        public string SFA3Nickname { get; set; }
+        public string SF4Nickname { get; set; }
+        public string SFA3Stage { get; set; }
+        public string SF2Stage { get; set; }
+        public string GolpesEspeciaisFamosos { get; set; }
+        public string Imagem { get; set; }
+    }
+}
