fc4b831c283f62fa4ebd94fbe7fec85dd7cf0f26
Unknown <maiconloebens@gmail.com>
2016-11-01 14:37:13 -0200

    fix(StreetFighter): corrigir dependencias.

--- src/modulo-05-dot-net/aula-03/StreetFighter.Web - Exercício/StreetFighter.Web/Controllers/StreetFighterController.cs
+++ src/modulo-05-dot-net/aula-03/StreetFighter.Web - Exercício/StreetFighter.Web/Controllers/StreetFighterController.cs
@@ -22,7 +22,7 @@ namespace StreetFighter.Web.Controllers
 
             personagem.Nome = "Blanka";
             personagem.PrimeiraAparicao = "Street Fighter II The World Warrior (1991)";
-            personagem.Nascimento = new DateTime(1966,2,12);
+            personagem.Nascimento = new DateTime(1966, 2, 12);
             personagem.Altura = 192;
             personagem.Peso = 96;
             personagem.Medidas = "B198, C120, Q172";
@@ -51,23 +51,22 @@ namespace StreetFighter.Web.Controllers
 
             personagem.Nome = "Akuma";
             personagem.PrimeiraAparicao = "Super Street Fighter II Turbo (1993)";
-            personagem.Nascimento = new DateTime(1966, 2, 12);
-            personagem.Altura = 192;
-            personagem.Peso = 96;
-            personagem.Medidas = "B198, C120, Q172";
-            personagem.TipoSanguineo = "B";
-            personagem.HabilidadesEspeciais = "Caçar, Eletricidade";
-            personagem.Gosta = "Frutas tropicais, Pirarucu, Sua mãe";
-            personagem.Desgosta = "Army ants (espécie de formiga)";
-            personagem.EstiloDeLuta = "Luta Selvagem autodidata (Army Ants) / Capoeira";
-            personagem.Origem = "Brasil (lugar de nascença é provável como sendo Tailândia)";
-            personagem.FalaDaVitoria = "Ver você em ação é uma piada!";
-            personagem.SFF2Nickname = "A selvagem criança da natureza";
-            personagem.SFA3Nickname = "A animal pessoa amazônica";
-            personagem.SF4Nickname = "Guerreiro da selva";
-            personagem.SFA3Stage = "Ramificação do Rio Madeira - pantano, Brasil (ramificação do rio Madeira: talvez possa ser Mato Grosso, ou Tocantins?)";
-            personagem.SF2Stage = "Bacia do rio Amazonas (Brasil)";
-            personagem.GolpesEspeciaisFamosos = "Electric Thunder, Rolling Attack";
+            personagem.Altura = 178;
+            personagem.Peso = 80;
+            personagem.Medidas = "B118, C84, Q86";
+            personagem.TipoSanguineo = "Desconhecido";
+            personagem.HabilidadesEspeciais = "Desconhecida";
+            personagem.Gosta = "Desconhecido";
+            personagem.Desgosta = "Desconhecido";
+            personagem.EstiloDeLuta = "Desconhecido (Goutetsu’s Ansatsuken maximizando Satsui no Hadou)";
+            personagem.Origem = "Japão";
+            personagem.FalaDaVitoria = "Agora, permaneça deitado!";
+            personagem.SFF2Nickname = "Desconhecido";
+            personagem.SFA3Nickname = "Desconhecido";
+            personagem.SF4Nickname = "Desconhecido";
+            personagem.SFA3Stage = "A Caverna de Kiga (Japão)";
+            personagem.SF2Stage = "Desconhecido";
+            personagem.GolpesEspeciaisFamosos = "Desconhecido";
 
             return View("FichaTecnica", personagem);
         }
--- src/modulo-05-dot-net/aula-03/StreetFighter.Web - Exercício/StreetFighter.Web/Properties/AssemblyInfo.cs
+++ src/modulo-05-dot-net/aula-03/StreetFighter.Web - Exercício/StreetFighter.Web/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d570b49e-d029-47a5-b85f-5dd34ec236e5")]
+[assembly: Guid("1ebddea4-2f33-469e-aabd-0057936ac684")]
 
 // Version information for an assembly consists of the following four values:
 //
