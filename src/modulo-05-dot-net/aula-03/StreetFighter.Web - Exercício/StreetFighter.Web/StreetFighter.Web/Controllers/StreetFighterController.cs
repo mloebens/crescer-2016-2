269568bb1ffcc61ce66f5abefecf3d500797a778
Máicon Loebens <maiconloebens@gmail.com>
2016-11-01 12:19:08 -0200

    feat(StreetFighter): implementar lógica - Incompleto.

--- src/modulo-05-dot-net/aula-03/StreetFighter.Web - Exercício/StreetFighter.Web/StreetFighter.Web/Controllers/StreetFighterController.cs
+++ src/modulo-05-dot-net/aula-03/StreetFighter.Web - Exercício/StreetFighter.Web/StreetFighter.Web/Controllers/StreetFighterController.cs
@@ -39,9 +39,37 @@ namespace StreetFighter.Web.Controllers
             personagem.SFA3Stage = "Ramificação do Rio Madeira - pantano, Brasil (ramificação do rio Madeira: talvez possa ser Mato Grosso, ou Tocantins?)";
             personagem.SF2Stage = "Bacia do rio Amazonas (Brasil)";
             personagem.GolpesEspeciaisFamosos = "Electric Thunder, Rolling Attack";
-            personagem.Imagem = "blanka.png";
 
-            return View();
+            return View(personagem);
+        }
+
+
+        // GET: Sobre
+        public ActionResult Sobre()
+        {
+            var personagem = new PersonagemModel();
+
+            personagem.Nome = "Akuma";
+            personagem.PrimeiraAparicao = "Super Street Fighter II Turbo (1993)";
+            personagem.Nascimento = new DateTime(1966, 2, 12);
+            personagem.Altura = 192;
+            personagem.Peso = 96;
+            personagem.Medidas = "B198, C120, Q172";
+            personagem.TipoSanguineo = "B";
+            personagem.HabilidadesEspeciais = "Caçar, Eletricidade";
+            personagem.Gosta = "Frutas tropicais, Pirarucu, Sua mãe";
+            personagem.Desgosta = "Army ants (espécie de formiga)";
+            personagem.EstiloDeLuta = "Luta Selvagem autodidata (Army Ants) / Capoeira";
+            personagem.Origem = "Brasil (lugar de nascença é provável como sendo Tailândia)";
+            personagem.FalaDaVitoria = "Ver você em ação é uma piada!";
+            personagem.SFF2Nickname = "A selvagem criança da natureza";
+            personagem.SFA3Nickname = "A animal pessoa amazônica";
+            personagem.SF4Nickname = "Guerreiro da selva";
+            personagem.SFA3Stage = "Ramificação do Rio Madeira - pantano, Brasil (ramificação do rio Madeira: talvez possa ser Mato Grosso, ou Tocantins?)";
+            personagem.SF2Stage = "Bacia do rio Amazonas (Brasil)";
+            personagem.GolpesEspeciaisFamosos = "Electric Thunder, Rolling Attack";
+
+            return View("FichaTecnica", personagem);
         }
     }
 }
