15e3468b9889ef5f875e12ec2b4c778137b77206
Unknown <maiconloebens@gmail.com>
2016-11-23 16:58:23 -0200

    feat(ItemParaEdicaoViewModel): adicionar validação.

--- src/modulo-07-reforco-dot-net/LojaDeItens/LojaDeItens.Web/Models/ItemMagico/ItemParaEdicaoViewModel.cs
+++ src/modulo-07-reforco-dot-net/LojaDeItens/LojaDeItens.Web/Models/ItemMagico/ItemParaEdicaoViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using LojaDeItens.Dominio.ItemMagico;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -29,12 +30,19 @@ namespace LojaDeItens.Web.Models.ItemMagico
         [StringLength(50)]
         public string Nome { get; set; }
 
+        [Required]
+        [DisplayName("Decrição")]
         public string Descricao { get; set; }
 
+        [Required]
+        [DisplayName("Preço")]
         public decimal Preco { get; set; }
 
+        [Required]
+        [Range(0, 50000, ErrorMessage = "A quantidade de estoque deve estar entre 0 e 50000")]
         public int Estoque { get; set; }
 
+        [Required]
         public bool Raro { get; set; }
     }
 }
