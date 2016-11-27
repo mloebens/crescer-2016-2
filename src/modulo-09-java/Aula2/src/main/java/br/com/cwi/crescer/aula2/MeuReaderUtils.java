65af001dbcd11c13ff11e650eb0d61b25d8b6335
Unknown <maiconloebens@gmail.com>
2016-11-27 19:00:29 -0200

    feat(aula2): implementar exercicio 2 e 3 de arquivos.

---
+++ src/modulo-09-java/Aula2/src/main/java/br/com/cwi/crescer/aula2/MeuReaderUtils.java
@@ -0,0 +1,33 @@
+package br.com.cwi.crescer.aula2;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Máicon Loebens
+ */
+public class MeuReaderUtils {
+
+    public void exibirConteudo(String nome) {
+        File arquivo = new File(nome);
+        if (arquivo.exists()) {
+            boolean ehTxt = nome.contains(".txt");
+            if (ehTxt) {
+                try {
+                    Files.readAllLines(Paths.get(nome)).forEach(System.out::println);
+                } catch (IOException ex) {
+                    System.out.println("Erro ao ler o arquivo.");
+                }
+            } else {
+                System.out.println("O arquivo é incompatível.");
+            }
+        } else {
+            System.out.println("Arquivo não existe.");
+        }
+    }
+}
---
+++ src/modulo-09-java/Aula2/src/main/java/br/com/cwi/crescer/aula2/MeuWriterUtils.java
@@ -0,0 +1,49 @@
+package br.com.cwi.crescer.aula2;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author Máicon Loebens
+ */
+public class MeuWriterUtils {
+
+    public static void main(String[] args) {
+        List<String> lista = Arrays.asList("teste", "teste1", "testeds3");
+        new MeuWriterUtils().gravarArquivo("teste.txt", lista);
+    }
+
+    public void gravarArquivo(String nome, List<String> conteudo) {
+        File arquivo = new File(nome);
+        if (arquivo.exists()) {
+            boolean ehTxt = nome.contains(".txt");
+            if (ehTxt) {
+                try (
+                        Writer writer = new FileWriter(nome, true);
+                        BufferedWriter bufferedWriter = new BufferedWriter(writer);
+                     ){
+                      for(String linha : conteudo){
+                          bufferedWriter.append(linha);
+                          bufferedWriter.newLine();
+                          
+                      }
+                      bufferedWriter.flush();
+                } catch (IOException ex) {
+                    System.out.println("Não foi possível gravar o arquivo.");
+                }
+            } else {
+                System.out.println("O arquivo é incompatível.");
+            }
+        } else {
+            System.out.println("Arquivo não existe.");
+        }
+    }
+}
