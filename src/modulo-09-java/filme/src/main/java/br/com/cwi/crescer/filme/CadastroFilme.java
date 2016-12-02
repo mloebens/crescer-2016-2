633c638bbc74f0e13eb9918b3c4410629cf71754
Unknown <maiconloebens@gmail.com>
2016-12-01 23:17:11 -0200

    feat(filme): implementar cadastro de filmes.

--- src/modulo-09-java/filme/src/main/java/br/com/cwi/crescer/filme/CadastroFilme.java
+++ src/modulo-09-java/filme/src/main/java/br/com/cwi/crescer/filme/CadastroFilme.java
@@ -1,7 +1,11 @@
 package br.com.cwi.crescer.filme;
 
 import br.com.cwi.crescer.filme.DAO.FilmeBean;
+import br.com.cwi.crescer.filme.Entity.Classificacao;
+import br.com.cwi.crescer.filme.Entity.Elenco;
 import br.com.cwi.crescer.filme.Entity.Filme;
+import br.com.cwi.crescer.filme.Entity.Genero;
+import br.com.cwi.crescer.filme.Entity.Idioma;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
@@ -17,6 +21,10 @@ public class CadastroFilme {
     
     private Filme filme;
     private List<Filme> filmes;
+    private Long idGenero;
+    private Long idClassificacao;
+    private Long idElenco;
+    private Long idIdioma;
 
     @PostConstruct
     public void init() {
@@ -41,7 +49,71 @@ public class CadastroFilme {
         this.filmes = filmes;
     }
 
+    public FilmeBean getFilmeBean() {
+        return filmeBean;
+    }
+
+    public void setFilmeBean(FilmeBean filmeBean) {
+        this.filmeBean = filmeBean;
+    }
+
+    public List<Filme> getFilmes() {
+        return filmes;
+    }
+
+    public void setFilmes(List<Filme> filmes) {
+        this.filmes = filmes;
+    }
+
+    public Long getIdClassificacao() {
+        return idClassificacao;
+    }
+
+    public void setIdClassificacao(Long idClassificacao) {
+        this.idClassificacao = idClassificacao;
+    }
+
+    public Long getIdElenco() {
+        return idElenco;
+    }
+
+    public void setIdElenco(Long idElenco) {
+        this.idElenco = idElenco;
+    }
+
+    public Long getIdIdioma() {
+        return idIdioma;
+    }
+
+    public void setIdIdioma(Long idIdioma) {
+        this.idIdioma = idIdioma;
+    }
+
+    public Long getIdGenero() {
+        return idGenero;
+    }
+
+    public void setIdGenero(Long idGenero) {
+        this.idGenero = idGenero;
+    }
+
+    
     public void adicionar() {
+        Genero genero = new Genero();
+        Classificacao classificacao = new Classificacao();
+        Elenco elenco = new Elenco();
+        Idioma idioma = new Idioma();
+        
+        genero.setId(idGenero);
+        classificacao.setId(idClassificacao);
+        elenco.setId(idElenco);
+        idioma.setId(idIdioma);
+        
+        filme.setGenero(genero);
+        filme.setClassificacao(classificacao);
+        filme.setElenco(elenco);
+        filme.setIdioma(idioma);
+        
         filmeBean.insert(filme);
         this.init();
     }
